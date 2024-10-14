521939f680e84b41cd4e4540c98d75d301e5ab4d
K-Schaeffer <kcb.schaeffer@gmail.com>
2024-10-14 19:50:04 -0300

    feat: set Inter as font

--- src/theme.ts
+++ src/theme.ts
@@ -4,7 +4,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   cssVariables: true,
   typography: {
-    fontFamily: 'var(--font-roboto)',
+    fontFamily: 'var(--font-family)',
   },
 });
 
