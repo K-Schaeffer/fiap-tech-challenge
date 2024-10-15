127920b41f75ec5766397393167943142e969def
K-Schaeffer <kcb.schaeffer@gmail.com>
2024-10-15 19:34:05 -0300

    feat: add dashboard route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={font.variable}>
         <AppRouterCacheProvider
           options={{ key: 'css' }}
