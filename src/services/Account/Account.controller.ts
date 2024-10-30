00d0fbbdde58f08e00bb96ea21e835d8f96ffbd9
K-Schaeffer <kcb.schaeffer@gmail.com>
2024-10-29 21:30:35 -0300

    feat: add account controlller

---
+++ src/services/Account/Account.controller.ts
@@ -0,0 +1,14 @@
+import { Account } from './Account.model';
+
+export const getAccountInfo = async () => {
+  const res = await fetch('http://localhost:3004/account', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+ 
+  const data: Account = await res.json();
+
+  return new Account(data);
+}
