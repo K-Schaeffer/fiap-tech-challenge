19733a90bc2b1307356b1c1cd97b2125407dd9be
K-Schaeffer <kcb.schaeffer@gmail.com>
2024-10-29 21:30:52 -0300

    feat: render Account info

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,17 @@
 import { Grid2, Typography } from "@mui/material";
 import styles from "./page.module.css";
 import FButton from "@/components/atoms/FButton/FButton";
+import { getAccountInfo } from "@/services/Account/Account.controller";
+import { Account } from "@/services/Account/Account.model";
 
-export default function Home() {
+export default async function Dashboard() {
+  const account: Account = await getAccountInfo();
+
   return (
-      <Grid2 container className={styles.page} spacing={2}>
+    <Grid2 container className={styles.page} spacing={2}>
+        { account.name }
+        { account.balance }
+        { account.currency }
         <main>
           <Typography variant="h1" color="primary">Foo</Typography>
           <Typography variant="subtitle1" color="secondary">Foo</Typography>
