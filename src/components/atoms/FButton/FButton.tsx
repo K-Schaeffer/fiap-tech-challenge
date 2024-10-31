9f4ac317bdebb9f88de411d8511b8f99a64d0bc1
K-Schaeffer <kcb.schaeffer@gmail.com>
2024-10-30 22:05:57 -0300

    feat: click handler to button

--- src/components/atoms/FButton/FButton.tsx
+++ src/components/atoms/FButton/FButton.tsx
@@ -3,11 +3,16 @@ import { Button, ButtonProps, Typography, Box } from "@mui/material";
 interface FButtonProps {
   innerText?: string;
   options?: ButtonProps;
+  onClick?: () => void;
 }
 
 export default function FButton(props: FButtonProps) {
   return (
-    <Button {...props.options} style={{textTransform: 'none', padding: 0, borderRadius: 8, maxHeight: 48 }}>
+    <Button 
+    {...props.options} 
+    style={{textTransform: 'none', padding: 0, borderRadius: 8, maxHeight: 48 }}
+    onClick={props.onClick}
+    >
       { props.innerText &&  
         <Box padding={2}>
           <Typography variant="body1" fontWeight={600}>
