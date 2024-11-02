99178b3b50647812997dc39cb4ffba052382f45e
Gabriel Oliveira <oliveira.gabriel@grupoboticario.com.br>
2024-11-02 19:34:26 -0300

    feat: create modal component

---
+++ src/components/organisms/FModal/FModal.styles.ts
@@ -0,0 +1,14 @@
+const modalContent = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "40%",
+  padding: "24px",
+  borderRadius: "8px",
+  backgroundColor: "var(--mui-palette-bgCard-main)",
+};
+
+const styles = { modalContent };
+
+export default styles;
