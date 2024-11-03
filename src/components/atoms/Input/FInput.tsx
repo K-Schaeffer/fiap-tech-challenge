d2aadc106cecf63825f0b71927e708e7bc5d4ce0
GustavoMartinsDev <gustavomartins.developer@gmail.com>
2024-11-03 16:07:46 -0300

    New Moleculas e atoms

--- src/components/atoms/Input/FInput.tsx
+++ src/components/atoms/Input/FInput.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Box, TextField } from '@mui/material';
+import { Box, TextField } from "@mui/material";
 
 interface InputProps {
   placeholder: string;
   textposition: "left" | "center";
   borderColor?: string;
-  maxWidth: string
+  labelInput?: string;
+  maxWidth?: string;
 }
 
-export default function Input({ placeholder, textposition, borderColor, maxWidth }: InputProps) {
+export default function Input({
+  placeholder,
+  textposition,
+  borderColor,
+  labelInput,
+  maxWidth,
+}: InputProps) {
   const isHexColor = borderColor! ?? "#004D61";
 
   return (
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { width: '100%', maxWidth: maxWidth, margin: "auto" },
+        "& > :not(style)": {
+          width: "100%",
+          margin: "auto",
+          maxWidth: maxWidth,
+        },
       }}
       noValidate
       autoComplete="off"
@@ -24,9 +34,10 @@ export default function Input({ placeholder, textposition, borderColor, maxWidth
         id="outlined-basic"
         variant="outlined"
         placeholder={placeholder}
+        label={labelInput}
         InputProps={{
           sx: {
-            '& input': {
+            "& input": {
               textAlign: textposition,
               backgroundColor: "#FFFFFF",
               color: isHexColor,
@@ -34,31 +45,32 @@ export default function Input({ placeholder, textposition, borderColor, maxWidth
               borderRadius: "8px",
               borderWidth: 1,
             },
-            '& .MuiOutlinedInput-notchedOutline': {
+            "& .MuiOutlinedInput-notchedOutline": {
               borderColor: isHexColor,
               borderRadius: "8px",
               borderWidth: 2,
             },
-            '& input::placeholder': {
+            "& input::placeholder": {
               color: isHexColor,
             },
-            '&:hover .MuiOutlinedInput-notchedOutline': {
+            "&:hover .MuiOutlinedInput-notchedOutline": {
               borderColor: isHexColor,
               borderWidth: 3,
             },
-            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
               borderColor: isHexColor,
               borderWidth: 3,
             },
-            '&.Mui-focused:hover .MuiOutlinedInput-notchedOutline': {
+            "&.Mui-focused:hover .MuiOutlinedInput-notchedOutline": {
               borderColor: isHexColor,
               borderWidth: 3,
             },
-            '&.Mui-focused:not(:hover):not(:focused) .MuiOutlinedInput-notchedOutline': {
-              borderColor: isHexColor,
-              borderWidth: 3,
-            },
-          }
+            "&.Mui-focused:not(:hover):not(:focused) .MuiOutlinedInput-notchedOutline":
+              {
+                borderColor: isHexColor,
+                borderWidth: 3,
+              },
+          },
         }}
       />
     </Box>
