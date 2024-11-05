975fc95c2a042a21705d5fb477b8b72b72d768b6
K-Schaeffer <kcb.schaeffer@gmail.com>
2024-11-05 19:32:40 -0300

    docs: add brand logo

---
+++ .storybook/manager.tsx
@@ -0,0 +1,9 @@
+import { addons } from "@storybook/manager-api";
+import { themes } from "@storybook/theming";
+
+addons.setConfig({
+  theme: {
+    ...themes.dark,
+    brandImage: "/assets/logo.svg",
+  },
+});
