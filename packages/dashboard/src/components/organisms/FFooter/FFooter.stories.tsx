58398f8b363bb9f2199b403c14903a3b22a513d7
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-01-14 22:33:32 -0300

    fix: prettier

--- packages/dashboard/src/components/organisms/FFooter/FFooter.stories.tsx
+++ packages/dashboard/src/components/organisms/FFooter/FFooter.stories.tsx
@@ -2,20 +2,24 @@ import FFooter from "@/components/organisms/FFooter/FFooter";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
-    title: "Organisms/Footer",
-    component: FFooter,
-    parameters: {
-        layout: "padded",
-        isDark: true
-    },
+  title: "Organisms/Footer",
+  component: FFooter,
+  parameters: {
+    layout: "padded",
+    isDark: true,
+  },
 } satisfies Meta<typeof FFooter>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        listService: ["Conta corrente", "Conta PJ", "Cartão de crédito"],
-        listContact: ["0800 004 250 08", "meajuda@bytebank.com.br", "ouvidoria@bytebank.com.br"],
-    },
+  args: {
+    listService: ["Conta corrente", "Conta PJ", "Cartão de crédito"],
+    listContact: [
+      "0800 004 250 08",
+      "meajuda@bytebank.com.br",
+      "ouvidoria@bytebank.com.br",
+    ],
+  },
 };
