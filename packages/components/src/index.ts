5dd3f8b459defa8262f4a961b7c21bc8a5dbb37d
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-01-19 01:29:46 -0300

    chore: add pages to alias on tsconfig

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,5 @@
 // export { NotFound } from "../pages/NotFound/NotFound.tsx";
-// export { AccountHome } from "../pages/AccountHome/AccountHome.tsx";
+// export { AccountHome } from "@pages/AccountHome/AccountHome.tsx";
 // export { AccountDashboard } from "../pages/AccountDashboard/AccountDashboard.tsx";
 
 export { FButton } from "@atoms/FButton/FButton.tsx";
