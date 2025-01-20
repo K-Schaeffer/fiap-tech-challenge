1567eadab9cd37cb0ee1d9919cd8abf12b72018a
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2025-01-19 21:39:22 -0300

    feat: creating filter

--- packages/components/src/organisms/FTransactionListCard/FTransactionListCard.tsx
+++ packages/components/src/organisms/FTransactionListCard/FTransactionListCard.tsx
@@ -1,15 +1,49 @@
+"use client"
+import { FIconButton } from "@atoms/FIconButton/FIconButton";
 import { FCard } from "@molecules/FCard/FCard";
+import FFilter from "@molecules/FFilter/FFilter";
 import {
   FTransactionList,
   FTransactionListProps,
 } from "@molecules/FTransactionList/FTransactionList";
+import { FilterList } from "@mui/icons-material";
 import { Container, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export function FTransactionListCard({
   transactionItems,
   editTransaction,
   deleteTransaction,
 }: FTransactionListProps) {
+
+  const [showSelect, setShowSelect] = useState(false);
+  const [filteredTransactions, setFilteredTransactions] = useState(transactionItems);
+  const [filterTypes, setFilterTypes] = useState<string[]>([]);
+
+  const toggleSelectVisibility = () => {
+    setShowSelect((prev) => !prev);
+  };
+
+
+  const handleFilterChange = (filterTypes: string[]) => {
+    setFilterTypes(filterTypes);
+  };
+
+  const applyFilter = () => {
+    if (filterTypes.length === 0) {
+      setFilteredTransactions(transactionItems);
+    } else {
+      const filtered = transactionItems.filter((transaction) =>
+        filterTypes.includes(transaction.type)
+      );
+      setFilteredTransactions(filtered);
+    }
+  };
+
+  useEffect(() => {
+    applyFilter();
+  }, [transactionItems, filterTypes]);
+
   return (
     <FCard>
       <Stack
@@ -21,6 +55,13 @@ export function FTransactionListCard({
         <Typography variant="h1" fontWeight={600}>
           Extrato
         </Typography>
+
+        <FIconButton
+          options={{ color: "primary" }}
+          onClick={toggleSelectVisibility}
+        >
+          <FilterList />
+        </FIconButton>
       </Stack>
       <Container
         sx={{
@@ -29,8 +70,22 @@ export function FTransactionListCard({
           overflowX: "hidden",
         }}
       >
+        <Stack
+          direction="row"
+          justifyContent="start"
+          alignItems="start"
+          paddingBottom={1}
+        >
+          <FFilter
+            onFilterChange={handleFilterChange}
+            showSelect={showSelect}
+          />
+
+        </Stack>
+
+
         <FTransactionList
-          transactionItems={transactionItems}
+          transactionItems={filteredTransactions}
           editTransaction={editTransaction}
           deleteTransaction={deleteTransaction}
         />
