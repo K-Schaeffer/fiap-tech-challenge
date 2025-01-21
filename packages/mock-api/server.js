4586c367f63e33cab8402aeaa1146d29ca37aa9c
GustavoMartinsDev <gustavomartins.developer@gmail.com>
2025-01-21 18:40:25 -0300

    Alteração api para porta 5000

--- packages/mock-api/server.js
+++ packages/mock-api/server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3006, () => {
-  console.log('JSON Server is running')
+server.listen(5000, () => {
+  console.log("JSON Server is running");
 });
