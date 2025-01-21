57eee188f28660b2230fae64dcf164cb76c6483c
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-01-21 20:14:21 -0300

    feat: setup module federation on apps

--- packages/dashboard-app/next.config.mjs
+++ packages/dashboard-app/next.config.mjs
@@ -1,4 +1,30 @@
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+// import CopyWebpackPlugin from "copy-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.output.publicPath = "auto";
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "dashboardApp",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./Index": "./src/pages/index.tsx",
+        },
+        shared: {},
+      })
+      // new CopyWebpackPlugin({
+      //   patterns: [{ from: "public/assets", to: "static/assets" }],
+      // })
+    );
+    return config;
+  },
+  reactStrictMode: true,
+  // images: {
+  //   domains: ["localhost"],
+  //   path: "/static/assets/",
+  // },
+};
 
 export default nextConfig;
