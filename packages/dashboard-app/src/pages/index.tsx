62ddb3b8e945a01420e739f832d0d27b0546bc24
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-01-21 23:52:26 -0300

    feat: integrate working microfrontends

--- packages/dashboard-app/src/pages/index.tsx
+++ packages/dashboard-app/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import AccountDashboard from "@/components/pages/AccountDashboard/AccountDashboard";
 import { MENU_ITEMS } from "@/constants/menuItems";
+import { getAccountInfo } from "@/services/Account/Account.controller";
 import { Account } from "@/services/Account/Account.model";
 import {
   addTransaction,
@@ -12,65 +13,36 @@ import {
   TransactionData,
   TransactionInput,
 } from "@/services/Transaction/Transaction.model";
-// import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useState } from "react";
 
-// function serializeAccount(account: Account) {
-//   return {
-//     ...account,
-//   };
-// }
-
-// function serializeTransactions(transactions: Transaction[]) {
-//   return transactions.map((transaction) => ({
-//     ...transaction,
-//   }));
-// }
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const account: Account = await getAccountInfo();
-//   const transactions: Transaction[] = await getTransactions();
-
-//   const serializedAccount = serializeAccount(account);
-//   const serializedTransactions = serializeTransactions(transactions);
-
-//   return {
-//     props: {
-//       account: serializedAccount,
-//       transactions: serializedTransactions,
-//     },
-//   };
-// };
-
-interface DashboardProps {
-  account: Account;
-  transactions: Transaction[];
-}
-
-export default function DashboardView(
-  {
-    // account,
-    // transactions,
-  }: DashboardProps
-) {
+export default function DashboardView() {
   const account: Account = {
-    fullName: "Joana da Silva Oliveira",
-    firstName: "Joana",
-    balance: 2500,
-    currency: "R$",
+    fullName: "",
+    firstName: "",
+    balance: 0,
+    currency: "",
   };
-  const transactions: Transaction[] = [
-    {
-      id: "f9fd",
-      type: "Transferência",
-      value: -100,
-      currency: "R$",
-      date: "2025-01-22T00:10:09.509Z",
-    },
-  ];
 
+  const transactions: Transaction[] = [];
+
+  const [localAccount, setLocalAccount] = useState(account);
   const [localTransactions, setLocalTransactions] = useState(transactions);
+
+  async function getInitialData() {
+    await fetchAccount();
+    await fetchTransactions();
+  }
+
+  async function fetchAccount() {
+    const updatedTransactions = await getAccountInfo();
+    setLocalAccount(updatedTransactions);
+  }
+
+  async function fetchTransactions() {
+    const updatedTransactions = await getTransactions();
+    setLocalTransactions(updatedTransactions);
+  }
 
   async function submitAddTransaction(transaction: TransactionInput) {
     await addTransaction(transaction);
@@ -101,7 +73,8 @@ export default function DashboardView(
       </Head>
       <AccountDashboard
         menuItems={MENU_ITEMS}
-        account={account}
+        account={localAccount}
+        getInitialData={getInitialData}
         transactionList={localTransactions}
         submitAddTransaction={submitAddTransaction}
         submitEditTransaction={submitEditTransaction}
--- packages/landing-app/src/pages/index.tsx
+++ packages/landing-app/src/pages/index.tsx
@@ -2,19 +2,9 @@ import AccountHome from "@/components/pages/AccountHome/AccountHome";
 import { FAccountButtonActions } from "components";
 import Head from "next/head";
 import { useRouter } from "next/router";
-// import { useEffect, useState } from "react";
 
 export default function HomeView() {
   const router = useRouter();
-  // const [isMounted, setIsMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
-
-  // if (!isMounted) {
-  //   return <div>Loading...</div>;
-  // }
 
   const actionsHome: FAccountButtonActions = {
     handleNewAccount: async () => {
