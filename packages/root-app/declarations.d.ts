e043a3095db3db96b23405419b0fd5b32acf37f4
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-01-22 21:32:29 -0300

    teste

--- packages/root-app/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "landingApp/Index" {
-  const Index: React.ComponentType;
-  export default Index;
-}
-
-declare module "dashboardApp/Index" {
-  const Index: React.ComponentType;
-  export default Index;
-}
